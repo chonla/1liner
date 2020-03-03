1373569d732067e734a57e6a05039f87cdfbbf3f
Chonlasith Jucksriporn <chonlasith@gmail.com>
2020-03-03 09:58:01 +0700

    Add node provisioning

---
+++ provision-node-latest.sh
@@ -0,0 +1,13 @@
+# Add PPA
+
+sudo apt-get install curl
+curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
+
+# Install Node
+
+sudo apt-get install nodejs
+
+# Test
+
+node -v
+npm -v
