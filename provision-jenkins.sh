20009eda90ff832933bfbebb5df224d2a7b83242
Chonlasith Jucksriporn <chonlasith@gmail.com>
2020-03-21 00:29:02 +0700

    Correct jenkins download

--- provision-jenkins.sh
+++ provision-jenkins.sh
@@ -2,7 +2,7 @@
 
 curl https://raw.githubusercontent.com/chonla/1liner/master/provision-docker.sh | bash
 
-curl -o jenkins.zip https://github.com/chonla/jenkins-with-docker-cli/archive/master.zip
+curl -o jenkins.zip https://codeload.github.com/chonla/jenkins-with-docker-cli/zip/master
 
 unzip ./jenkins.zip /opt/jenkins
 
