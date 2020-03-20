5f0ed2fda71dacfc7c5009f5521486c79475fe3e
Chonlasith Jucksriporn <chonlasith@gmail.com>
2020-03-21 00:32:05 +0700

    Fix install jenkins

--- provision-jenkins.sh
+++ provision-jenkins.sh
@@ -4,8 +4,8 @@ curl https://raw.githubusercontent.com/chonla/1liner/master/provision-docker.sh 
 
 curl -o jenkins.zip https://codeload.github.com/chonla/jenkins-with-docker-cli/zip/master
 
-unzip ./jenkins.zip /opt/jenkins
+unzip ./jenkins.zip -d /opt
 
-cd /opt/jenkins
+cd /opt/jenkins-with-docker-cli-master
 
 sudo docker-compose up --build -d
